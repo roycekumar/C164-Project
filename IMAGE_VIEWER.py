eb86a073074dcf57f2b5f263102b6d20b3dcfe65
Royce Kumar <69853766+roycekumar@users.noreply.github.com>
2021-01-30 11:13:33 +0530

    Create  IMAGE_VIEWER.py

---
+++ IMAGE_VIEWER.py
@@ -0,0 +1,18 @@
+from tkinter import *
+from tkinter import filedialog
+from PIL import ImageTk,Image
+root=Tk()
+root.title("Planet Encyclopedia")
+root.geometry("600x600")
+root.configure(background="black")
+def Open():
+    print("open")
+def rotate():
+    print("Rotate")
+btn_open=Button(root,text="Open Image",command=Open,relief="flat",font=("Times New Roman0",15),bg="#808080",fg="white")
+btn_open.place(relx=0.5,rely=0.08,anchor=CENTER)
+btn_rotate=Button(root,text="Rotate Image",command=rotate,relief="flat",font=("Times New Roman0",15),bg="#808080",fg="white")
+btn_rotate.place(relx=0.5,rely=0.8,anchor=CENTER)
+label_image=Label(root,text="royce",bg="black",fg="black")
+label_image.pack()
+root.mainloop()
